cd57cd42a601e25c30a1189b217823f966291dfc
eberry <>
2013-02-22 12:18:36 +0000

    Script to make JSONs for splitting 2012B, C, D into manageable sizes for CRAB

---
+++ make_jsons.sh
@@ -0,0 +1,15 @@
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 193833 --max 194832 --output json_2012B_193833-194832_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 194833 --max 195832 --output json_2012B_194833-195832_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 195833 --max 196531 --output json_2012B_195833-196531_DCSONLY.txt
+
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 198934 --max 199933 --output json_2012C_198934-199933_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 199934 --max 200933 --output json_2012C_199934-200933_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 200934 --max 201933 --output json_2012C_200934-201933_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 201934 --max 202933 --output json_2012C_201934-202933_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 202934 --max 203746 --output json_2012C_202934-203746_DCSONLY.txt
+
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 203768 --max 204767 --output json_2012D_203768-204767_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 204768 --max 205767 --output json_2012D_204768-205767_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 205768 --max 206767 --output json_2012D_205768-206767_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 206768 --max 207767 --output json_2012D_206768-207767_DCSONLY.txt
+filterJSON.py /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt --min 207768 --max 208686 --output json_2012D_207768-208686_DCSONLY.txt
