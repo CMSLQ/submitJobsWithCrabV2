30ca675a131dfb7d2ca72d38e580a33d67af1e9d
Seth Cooper <seth.cooper@cern.ch>
2016-01-28 20:33:51 +0100

    Prepare to setup lhapdf (but don't do it).

--- user_script.sh
+++ user_script.sh
@@ -1,3 +1,6 @@
-scram setup lhapdf
-scram b
-cmsRun -j FrameworkJobReport.xml -p pset.py
+#echo "CMSSW_BASE=$CMSSW_BASE"
+#cd $CMSSW_BASE/src
+#scram setup lhapdf
+#scram b
+#cd -
+cmsRun -j FrameworkJobReport.xml -p PSet.py
