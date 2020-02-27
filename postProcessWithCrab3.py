40f6509ac637e2a142ad71f465889de9b825dd8d
Seth Cooper <seth.cooper@cern.ch>
2020-02-27 15:37:13 +0100

    add prefire correction to doSkim and fix up crab script

--- postProcessWithCrab3.py
+++ postProcessWithCrab3.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
+from __future__ import print_function
 import subprocess
 import os
 import sys
@@ -12,8 +13,8 @@ try:
     from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 except ImportError:
     print
-    print 'ERROR: Could not load CRABClient.UserUtilities.  Please source the crab3 setup:'
-    print 'source /cvmfs/cms.cern.ch/crab3/crab.sh'
+    print('ERROR: Could not load CRABClient.UserUtilities.  Please source the crab3 setup:')
+    print('source /cvmfs/cms.cern.ch/crab3/crab.sh')
     exit(-1)
 try:
     cmsswBaseDir = os.environ['CMSSW_BASE']
@@ -31,9 +32,15 @@ def crabSubmit(config):
         crabCommand('submit', config = config)
         #crabCommand('submit', 'dryrun', config = config)
     except HTTPException, hte:
-        print '-----> there was a problem. see below.'
-        print hte.headers
-        print 'quit here'
+        sys.stdout.write("\033[1;31m") # red
+        print('-----> there was a problem. see below.')
+        print(hte.headers)
+        print('quit here')
+        fileName = 'failedCrabCfg.py'
+        with open(fileName,'w') as f:
+           print(config, file=f)
+        print('configuration saved to',fileName)
+        sys.stdout.write("\033[0;0m") # regular color
         q.put(-1)
     q.put(0)
     
@@ -44,11 +51,11 @@ def validateOptions(options):
     elif options.inputList is None:
         error = True
     if error:
-        print 'You are missing one or more required options: d, i'
+        print('You are missing one or more required options: d, i')
         parser.print_help()
         exit(-1)
     if options.prevJsonFile is not None and options.jsonFile is None:
-        print 'It does not make sense to specify a previously used/analyzed JSON file without specifying a new JSON file, since with this option specified, the difference between the new and old JSON is taken as the lumi mask.'
+        print('It does not make sense to specify a previously used/analyzed JSON file without specifying a new JSON file, since with this option specified, the difference between the new and old JSON is taken as the lumi mask.')
         exit(-1)
 
 
@@ -57,7 +64,7 @@ def makeDirAndCheck(dir):
         os.makedirs(dir)
     else:
         # in practice, this doesn't happen because of the seconds in the name, but always good to check
-        print 'ERROR: directory %s already exists. Not going to overwrite it.' % dir
+        print('ERROR: directory %s already exists. Not going to overwrite it.' % dir)
         exit(-2)
 
 
@@ -68,7 +75,7 @@ def CheckProxy():
     #print 'err------>',err
     if 'Proxy not found' in err or 'timeleft  : 00:00:00' in out:
         # get a proxy
-        print 'you have no valid proxy; let\'s get one via voms-proxy-init:'
+        print('you have no valid proxy; let\'s get one via voms-proxy-init:')
         # this will suppress the stderr; maybe that's not so good, but I get some error messages at the moment
         #with open(os.devnull, "w") as f:
         #  proc2 = subprocess.call(['voms-proxy-init','--voms','cms','--valid','168:00'],stderr=f)
@@ -76,15 +83,15 @@ def CheckProxy():
  
 
 def checkStoragePath(storagePath):
-    print 'will store (example):',storagePath
+    print('will store (example):',storagePath)
     #print '\twhich has length:',len(storagePath)
     if len(storagePath) > 255:
       print
-      print 'we might have a problem with output path lengths too long (if we want to run crab over these).'
-      print 'example output will look like:'
-      print storagePath
-      print 'which has length:',len(storagePath)
-      print 'cowardly refusing to submit the jobs; exiting'
+      print('we might have a problem with output path lengths too long (if we want to run crab over these).')
+      print('example output will look like:')
+      print(storagePath)
+      print('which has length:',len(storagePath))
+      print('cowardly refusing to submit the jobs; exiting')
       exit(-3)
     #else:
     #  print
@@ -170,9 +177,9 @@ cfgFilesDir = productionDir+'/cfgfiles'
 outputDir = productionDir+'/output'
 workDir = productionDir+'/workdir'
 localDirs = [productionDir,cfgFilesDir,outputDir,workDir]
-print 'Making local directories:'
+print('Making local directories:')
 for dir in localDirs:
-  print '\t',dir
+  print('\t',dir)
   makeDirAndCheck(dir)
 print
 
@@ -230,9 +237,9 @@ if options.eosDir is not None:
   if options.eosDir.startswith('/eos/cms'):
     options.eosDir = options.eosDir.split('/eos/cms')[-1]
   # require /store unless it's CERNBOX
-  if options.storageSite!='T2_CH_CERNBOX' and not options.eosDir.startswith('/store'):
-    print 'eosDir must start with /eos/cms/store or /store and you specified:',options.eosDir
-    print 'quit'
+  if not options.eosDir.startswith('/store'):
+    print('eosDir must start with /eos/cms/store or /store and you specified:',options.eosDir)
+    print('quit')
     exit(-1)
   outputLFN=options.eosDir
   if not outputLFN[-1]=='/':
@@ -249,7 +256,7 @@ if options.eosDir is not None:
       config.Data.outLFNDirBase = outputLFN+'/%s/' % (getUsernameFromSiteDB()) + '/'
   else:
     config.Data.outLFNDirBase = outputLFN
-print 'Using outLFNDirBase:',config.Data.outLFNDirBase
+print('Using outLFNDirBase:',config.Data.outLFNDirBase)
 config.Site.storageSite = options.storageSite
 
 # look at the input list
@@ -265,7 +272,7 @@ with open(localInputListFile, 'r') as f:
       #print 'found comment:',line
       continue
     if len(split) < 3:
-      print 'inputList line is not properly formatted:',line
+      print('inputList line is not properly formatted:',line)
       exit(-3)
     dataset = split[0]
     nUnits = int(split[1]) #also used for total lumis for data
@@ -276,7 +283,7 @@ with open(localInputListFile, 'r') as f:
     config.Data.outputDatasetTag=datasetTag
     config.Data.inputDataset = dataset
     print
-    print 'Consider dataset {0}'.format(dataset)
+    print('Consider dataset {0}'.format(dataset))
 
     if not isData:
       config.Data.splitting = 'FileBased'
@@ -293,7 +300,7 @@ with open(localInputListFile, 'r') as f:
     elif 'Autumn18' in secondaryDatasetName or 'Run2018' in secondaryDatasetName:
       year=2018
     else:
-      print 'ERROR: could not determine year from secondaryDatasetName "{0}" from datasetName "{1}"'.format(secondaryDatasetName,datasetName)
+      print('ERROR: could not determine year from secondaryDatasetName "{0}" from datasetName "{1}"'.format(secondaryDatasetName,datasetName))
       exit(-4)
     # get dataRun
     dataRun = 'X'
@@ -315,7 +322,7 @@ with open(localInputListFile, 'r') as f:
     # computing JSON mask
     if options.jsonFile is not None:
       if options.prevJsonFile is not None:
-        print 'Using the subtraction between previous json and new json; WARNING: if lumis changed from good in previous to bad in new json, this will not remove them'
+        print('Using the subtraction between previous json and new json; WARNING: if lumis changed from good in previous to bad in new json, this will not remove them')
         from WMCore.DataStructs.LumiList import LumiList
         prevJsonLumiList = LumiList(url=options.prevJsonFile) if 'http:' in options.prevJsonFile else LumiList(filename=options.prevJsonFile)
         currentJsonLumiList = LumiList(url=options.jsonFile) if 'http:' in options.jsonFile else LumiList(filename=options.jsonFile)
@@ -329,9 +336,9 @@ with open(localInputListFile, 'r') as f:
       config.Data.runRange = runRange
 
     # and submit
-    print config.JobType.scriptArgs
-    print 'submit to crab. output from crab submit follows:'
-    sys.stdout.write("\033[1;34m")
+    print(config.JobType.scriptArgs)
+    print('submit to crab. output from crab submit follows:')
+    sys.stdout.write("\033[1;34m") # blue
     #crabSubmit(config)
     # workaround for cmssw multiple-loading problem
     # submit in subprocess
@@ -342,10 +349,10 @@ with open(localInputListFile, 'r') as f:
     if q.get()==-1:
       exit(-1)
     sys.stdout.write("\033[0;0m")
-    print 'Done with this dataset.'
+    print('Done with this dataset.')
     
 
-print 'Done!' 
+print('Done!')
 exit(0)
 
 
